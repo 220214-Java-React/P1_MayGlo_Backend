cd4af69753d82298c8faa0d237f9726d3eb651cf
Marcus Glover <marcus961@revature.net>
2022-03-22 18:27:49 -0400

    Added Reimbursement class

---
+++ src/main/java/dev/mayglo/Reimbursement.java
@@ -0,0 +1,17 @@
+package dev.mayglo;
+
+public class Reimbursement
+{
+    // Reimbursement objects contain basic details.
+
+    // Type:
+    //      - Lodging
+    //      - Travel
+    //      - Food
+    //      - Other
+
+    // Status:
+    //      - Pending
+    //      - Approved
+    //      - Denied
+}
