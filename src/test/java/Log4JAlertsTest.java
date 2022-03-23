2fc89d828bd8166c81ab0327516708042cd60e23
Asheton S. M <AshetonSM@gmail.com>
2022-03-23 13:14:35 -0500

    Changed log4j2.xml so that the file clears itself instead of appending on each run.

--- src/test/java/Log4JAlertsTest.java
+++ src/test/java/Log4JAlertsTest.java
@@ -1,5 +1,6 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
