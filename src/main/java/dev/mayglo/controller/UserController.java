36ad0b6abedbe204882783ac11cad35c12836fef
Marcus Glover <marcus961@revature.net>
2022-03-23 14:58:42 -0400

    Renamed class to controller variation

--- src/main/java/dev/mayglo/controller/UserController.java
+++ src/main/java/dev/mayglo/controller/UserController.java
@@ -1,12 +1,14 @@
-package dev.mayglo.servlets;
+package dev.mayglo.controller;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class UserServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/hello")
+public class UserController extends HttpServlet {
 
     /**
      * Requesting a resource
