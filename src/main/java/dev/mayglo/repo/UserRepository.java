928f46a10bf626b09550b83298f7cd9354791fe3
Asheton S. M <AshetonSM@gmail.com>
2022-03-24 17:00:06 -0500

    Added getByID and changed some values to values from DatabaseRef.

--- src/main/java/dev/mayglo/repo/UserRepository.java
+++ src/main/java/dev/mayglo/repo/UserRepository.java
@@ -1,5 +1,6 @@
 package dev.mayglo.repo;
 
+import dev.mayglo.DAO.DatabaseRef;
 import dev.mayglo.DAO.MainDAO;
 import dev.mayglo.model.User;
 import dev.mayglo.util.ConnectionFactory;
@@ -8,10 +9,11 @@ import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-public class UserRepository implements MainDAO<User> {
+public class UserRepository implements MainDAO<User>, DatabaseRef {
 
     private static final Logger logger = LogManager.getLogger(UserRepository.class.getName());
 
@@ -21,8 +23,9 @@ public class UserRepository implements MainDAO<User> {
 
         try {
             connection = ConnectionFactory.getConnection();
-            String sql = "insert into ERS_USER (username, password, email, given_name, " +
-                    "surname, is_active, role_id) values (?, ?, ?, ?, ?, ?, ?)";
+            String sql = String.format("insert into %s (%s, %s, %s, %s, " +
+                            "%s, %s, %s) values (?, ?, ?, ?, ?, ?, ?)", USER_TABLE, COL_USER_USERNAME, COL_USER_PASSWORD,
+                    COL_USER_EMAIL, COL_USER_GIVEN_NAME, COL_USER_SURNAME, COL_USER_IS_ACTIVE,COL_USER_ROLE_ID);
 
 
             PreparedStatement stmt = connection.prepareStatement(sql);
@@ -53,8 +56,37 @@ public class UserRepository implements MainDAO<User> {
         return null;
     }
 
-    public User getByID(Integer i) {
-        return null;
+    public User getByID(Integer id) {
+
+        User user = null;
+        String sql = "select * from ers_user where id = ?";
+        Connection connection;
+
+        try{
+            connection = ConnectionFactory.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql);
+
+            stmt.setInt(1, id);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if(rs.next()){
+                user = new User(
+                        rs.getInt("user_id"),
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("email"),
+                        rs.getString("given_name"),
+                        rs.getString("surname"),
+                        rs.getBoolean("is_active"),
+                        rs.getInt("role_id")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return user;
     }
 
     @Override
