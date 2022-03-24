97d1369c68e34d3a55f9066a8db6ea5c62472ef4
Marcus Glover <marcus961@revature.net>
2022-03-24 17:14:28 -0400

    Created DatabaseRef interface to maintain consistency with Database referencing

--- src/main/java/dev/mayglo/repo/ReimbRepository.java
+++ src/main/java/dev/mayglo/repo/ReimbRepository.java
@@ -1,16 +1,30 @@
 package dev.mayglo.repo;
 
+import dev.mayglo.DAO.DatabaseRef;
 import dev.mayglo.DAO.MainDAO;
 import dev.mayglo.model.Reimbursement;
+import dev.mayglo.util.ConnectionFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
-public class ReimbRepository implements MainDAO<Reimbursement>
+public class ReimbRepository implements MainDAO<Reimbursement>, DatabaseRef
 {
+    private static final Logger logger = LogManager.getLogger(ReimbRepository.class);
+
     @Override
     public void create(Reimbursement reimbursement)
     {
-
+        try (Connection connector = ConnectionFactory.getConnection())
+        {
+        }
+        catch (SQLException e)
+        {
+            logger.warn(e);
+        }
     }
 
     @Override
