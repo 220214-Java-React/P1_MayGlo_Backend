311c20a9ef23c4992bc41f93a95c17a6f2e25fa0
Marcus Glover <marcus961@revature.net>
2022-03-24 18:27:13 -0400

    Added create method to ReimbService in order to access ReimbRepository's respective method

--- src/main/java/dev/mayglo/service/ReimbService.java
+++ src/main/java/dev/mayglo/service/ReimbService.java
@@ -1,5 +1,20 @@
 package dev.mayglo.service;
 
+import dev.mayglo.model.Reimbursement;
+import dev.mayglo.repo.ReimbRepository;
+
 public class ReimbService
 {
+    private final ReimbRepository reimbRepository;
+
+    public ReimbService()
+    {
+        this.reimbRepository = new ReimbRepository();
+    }
+
+    public void create(Reimbursement reimbursement)
+    {
+        reimbRepository.createReimbursement(reimbursement);
+    }
+
 }
