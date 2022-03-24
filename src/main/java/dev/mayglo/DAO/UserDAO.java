902d7ef9bfc93f27f6bf4572a62dcac978828622
Asheton S. M <AshetonSM@gmail.com>
2022-03-23 21:48:48 -0500

    Added a DAO folder and a temporary UserDAO interface.

---
+++ src/main/java/dev/mayglo/DAO/UserDAO.java
@@ -0,0 +1,30 @@
+package dev.mayglo.DAO;
+
+import java.util.List;
+
+public interface UserDAO<T> {
+
+    // Create a User
+    void create(T t);
+
+    // Read/Get (self-get, for use by current user?)
+    T get(T t);
+    
+    // Read/Get by ID
+    T getByID(Integer i);
+
+    // Get a List of all users
+    List<T> getAll();
+
+    // Update a User (for use by current user?)
+    void update(T t);
+
+    // Update a User by ID (for use by manager and admins?)
+    void updateByID(Integer i);
+
+    // Delete a User
+    void delete(T t);
+
+    // Delete a User by ID
+    void delete(String s);
+}
