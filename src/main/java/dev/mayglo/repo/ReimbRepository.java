fc0fb5fac377ceba8360bce0ec0218aacc8797f1
Marcus Glover <marcus961@revature.net>
2022-03-24 17:51:56 -0400

    Implemented a create method for database persistence with reimbursements

--- src/main/java/dev/mayglo/repo/ReimbRepository.java
+++ src/main/java/dev/mayglo/repo/ReimbRepository.java
@@ -3,11 +3,13 @@ package dev.mayglo.repo;
 import dev.mayglo.DAO.DatabaseRef;
 import dev.mayglo.DAO.MainDAO;
 import dev.mayglo.model.Reimbursement;
+import dev.mayglo.model.User;
 import dev.mayglo.util.ConnectionFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -15,15 +17,34 @@ public class ReimbRepository implements MainDAO<Reimbursement>, DatabaseRef
 {
     private static final Logger logger = LogManager.getLogger(ReimbRepository.class);
 
+    private static String query;
+    private static String columnsToQuery;
+
     @Override
     public void create(Reimbursement reimbursement)
     {
-        try (Connection connector = ConnectionFactory.getConnection())
+    }
+
+    public void createReimbursement(Reimbursement reimbursement, User user)
+    {
+        try(Connection connection = ConnectionFactory.getConnection())
         {
+            // Columns to query
+            columnsToQuery = "(" + COL_REIMB_AMT + ", " + COL_REIMB_SUBMITTED + ", " + COL_REIMB_AUTHOR_ID + ")";
+
+            // insert query to reimbursement table
+            query = "insert into " + REIMB_TABLE + columnsToQuery + " values (?, now(), ?)";
+
+            PreparedStatement stmt = connection.prepareStatement(query);
+
+            stmt.setDouble(1, reimbursement.getAmount());
+            stmt.setInt(2, user.getID());
+
+            stmt.executeUpdate();
         }
-        catch (SQLException e)
+        catch (SQLException sqlE)
         {
-            logger.warn(e);
+            logger.warn(sqlE);
         }
     }
 
