b718b2e8b3ebea3a1d0971d801c3c868b311b6e0
Marcus Glover <marcus961@revature.net>
2022-03-28 17:20:27 -0400

    Added response codes for returning to JS

--- src/main/java/dev/mayglo/controller/LoginController.java
+++ src/main/java/dev/mayglo/controller/LoginController.java
@@ -31,28 +31,30 @@ public class LoginController extends HttpServlet
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-
-
         // Extract the request payload in JSON form from the BufferedReader on the request object
         String JSON = req.getReader().lines().collect(Collectors.joining());
-        User user = null;
+        User user;
 
         // We unmarshall the JSON string into a Java instance of the User class
         try {
-            user = mapper.readValue(JSON, User.class);
+            user = mapper.readValue(JSON, User.class);  // Map request info (JSON) to a User object
 
-            User DBUser = userService.getByUsername(user.getUsername());
+            User DBUser = userService.getByUsername(user.getUsername());    // Find a matching username in database
 
-            if (DBUser != null && userService.checkUser(DBUser, user))
+            if (DBUser != null )      // If a user is found
             {
-                logger.info("Found: " + DBUser);
+                if ( userService.checkUser(DBUser, user))   // If the password matches
+                {
+                    logger.info("Found: " + DBUser);
 
-                // 200 - OK, 201 - Created is good if you're returning, 204 - No Content
-                resp.setStatus(204);
+                    if (DBUser.getRole_ID() == 0) resp.setStatus(200);
+                    else if (DBUser.getRole_ID() == 1) resp.setStatus(201);
+                    else if (DBUser.getRole_ID() == 2) resp.setStatus(202);
+                }
             }
-            else
+            else    // Otherwise
             {
-                resp.setStatus(205);
+                resp.setStatus(205);    // Another code that isn't listed above
             }
 
 //            List<User> users = userService.getAll();
